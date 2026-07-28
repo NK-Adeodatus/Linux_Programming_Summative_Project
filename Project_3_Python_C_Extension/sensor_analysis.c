d59524f0e0a2709aa1e7545f9d2ab297e5d8fe37
Adeodatus Nkundimana <a.nkundiman@alustudent.com>
2026-07-28 12:18:21 +0200

    add validation for the data set

--- Project_3_Python_C_Extension/sensor_analysis.c
+++ Project_3_Python_C_Extension/sensor_analysis.c
@@ -1,6 +1,27 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
+/*
+ * Helper function: validate_and_get_size
+ * Checks if the input is a list or tuple, ensures it is not empty,
+ * and returns the size. Returns -1 if validation fails and sets a Python exception.
+ */
+static Py_ssize_t validate_and_get_size(PyObject *data) {
+    if (!PyList_Check(data) && !PyTuple_Check(data)) {
+        PyErr_SetString(PyExc_TypeError, "Input data must be a list or tuple.");
+        return -1;
+    }
+
+    Py_ssize_t size = PySequence_Size(data);
+    
+    if (size == 0) {
+        PyErr_SetString(PyExc_ValueError, "Dataset cannot be empty.");
+        return -1;
+    }
+    
+    return size;
+}
+
 /* 
  * Method table: maps Python function names to C functions.
  * Currently empty, we will add our statistical functions here later.
