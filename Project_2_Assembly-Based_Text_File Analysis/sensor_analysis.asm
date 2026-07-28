9a77c079dbfaec782c93248ae1f054403b3f8a57
Adeodatus Nkundimana <a.nkundiman@alustudent.com>
2026-07-28 09:56:11 +0200

    add functionality to read the txt file

--- Project_2_Assembly-Based_Text_File Analysis/sensor_analysis.asm
+++ Project_2_Assembly-Based_Text_File Analysis/sensor_analysis.asm
@@ -2,9 +2,11 @@ section .data
     filename db "sensor_readings.txt", 0
     err_open_msg db "Error: Cannot open file", 10
     err_open_len equ $ - err_open_msg
+    err_read_msg db "Error: Cannot read file", 10
+    err_read_len equ $ - err_read_msg
 
 section .bss
-    ; Uninitialized data will go here (e.g., our file read buffer)
+    buffer resb 4096    ; reserve 4096 bytes for file content
 
 section .text
     global _start
@@ -22,6 +24,22 @@ _start:
     jl .open_error
 
     ; (File descriptor is safe in rax, ready for reading in the next step)
+    mov r12, rax        ; save file descriptor in r12
+
+    ; 3. Read the file
+    mov rax, 0          ; sys_read system call number
+    mov rdi, r12        ; file descriptor
+    mov rsi, buffer     ; pointer to our memory buffer
+    mov rdx, 4096       ; maximum number of bytes to read
+    syscall
+
+    ; 4. Check for error (bytes read in rax < 0)
+    cmp rax, 0
+    jl .read_error
+
+    ; Save the number of bytes read in r13 for traversal later
+    mov r13, rax
+
     jmp .exit_success
 
 .open_error:
@@ -35,6 +53,19 @@ _start:
     ; 4. Exit with error status
     mov rax, 60         ; sys_exit system call number
     mov rdi, 1          ; exit status 1 (error)
+    syscall
+
+.read_error:
+    ; Print read error message
+    mov rax, 1          ; sys_write
+    mov rdi, 1          ; stdout
+    mov rsi, err_read_msg
+    mov rdx, err_read_len
+    syscall
+
+    ; Exit with error status
+    mov rax, 60
+    mov rdi, 1
     syscall
 
 .exit_success:
